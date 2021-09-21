a17897d97753488d8f019e777318e59581726290
Bradley Rupczyk <support@zykdesigns.com>
2021-09-21 18:05:50 -0400

    Landing page finished

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,24 @@
+import logo from '../images/LogoBlack.svg'
+import signature from '../images/Signature.png'
+
 const Landing = () => {
     return (
-        <div>
-            Landing page
+        <div className="centered-page">
+            <img className="signature" src={signature} alt="" />
+            <div className="landing">
+                <div className="welcome-area">
+                    <img className="logo" src={logo} alt=""/>
+                    <p className="welcome-msg">
+                    Premium digital graphic designs for posters, business cards, and more.
+                    <br /><br />
+                    by Bradley Rupczyk
+                    </p>
+                </div>
+                <div className="button-area">
+                    <button className="button-1">See Examples</button>
+                    <button className="button-1">Order a Design</button>
+                </div>
+            </div>
         </div>
     )
 }
